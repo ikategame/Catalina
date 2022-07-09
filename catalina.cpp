30ef5f3a58fc28d946cbfa0d9eb7653f9402a11f
Catalina <eunightcoredesire@gmail.com>
2022-07-09 10:19:36 +0300

    g

--- catalina.cpp
+++ catalina.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <thread>
 #include "io.h"
 #include "Log.h"
+
+void game()
+{
+	int HP = 100;
+	while (true)
+	{
+		HP--;
+		std::cout << HP << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
+}
 
 int main()
 {
 	std::cout << "IKategame" << std::endl;
+	game();
 	int x = getNuM();
 	int y = getNuM();
 	writeNUm(x + y);
