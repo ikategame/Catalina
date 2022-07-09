30b645f293986a616760b466110d38969b764ba5
Catalina <eunightcoredesire@gmail.com>
2022-07-09 12:21:29 +0300

    12

--- catalina.cpp
+++ catalina.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <thread>
+#include <Windows.h>
+#include <string>
 #include "io.h"
 #include "Log.h"
 
@@ -14,13 +16,36 @@ void game()
 	}
 }
 
+int MEMusage()
+{
+	MEMORYSTATUSEX memInfo;
+	memInfo.dwLength = sizeof(MEMORYSTATUSEX);
+	GlobalMemoryStatusEx(&memInfo);
+	return memInfo.dwMemoryLoad;
+}
+
+void Print(std::string& text)
+{
+	std::cout << text << std::endl;
+}
+
 int main()
 {
+	int MEM = MEMusage();
+	std::string A = "asd";
+	Print(A);
+
+	while (true)
+	{
+		std::cout << "MEM: " << MEM << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
 	Log("IKategame");
-	game();
+
 	int x = getNuM();
 	int y = getNuM();
 	writeNUm(x + y);
+
 }
 
 
